1fb44cc46f4baed00f17df017b7e29ff89d1265b
Tyler Liu <tyler4long@gmail.com>
2023-03-11 17:23:39 -0800

    Provide tree structure

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,38 @@ import materialIcons from './material-icons';
 
 const icons = [...fontawesome, ...materialIcons];
 
+function convertArrayToObject(arr: string[][]): any {
+  const obj: any = {};
+  for (const subArr of arr) {
+    let currentNode = obj;
+    for (const value of subArr) {
+      if (!currentNode[value]) {
+        currentNode[value] = {};
+      }
+      currentNode = currentNode[value];
+    }
+  }
+  return obj;
+}
+
+interface TreeNode {
+  value: string;
+  children?: TreeNode[];
+}
+
+function convertObjectToTree(obj: any): TreeNode[] {
+  const tree: TreeNode[] = [];
+  for (const key of Object.keys(obj)) {
+    const node: TreeNode = { value: key };
+    const children = obj[key];
+    if (children && typeof children === 'object' && Object.keys(children).length > 0) {
+      node.children = convertObjectToTree(children);
+    }
+    tree.push(node);
+  }
+  return tree;
+}
+
+export const tree = convertObjectToTree(convertArrayToObject(icons.map((icon) => icon.cascade)));
+
 export default icons;
