d60edd8f1f5fd3a4a900abf1311710028e2b8807
Tyler Liu <tyler4long@gmail.com>
2023-03-12 13:54:01 -0700

    Sort the icons

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import materialIcons from './generated/material-icons';
 import { convertArrayToTree, TreeNode } from './utils';
 
 const icons = [...fontawesome, ...materialIcons];
+icons.sort((a, b) => {
+  return a.cascade.join('').localeCompare(b.cascade.join(''));
+});
 
 export const tree: TreeNode[] = convertArrayToTree(icons.map((icon) => icon.cascade));
 
